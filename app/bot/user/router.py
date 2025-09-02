6b82a1bf172e18a3d07dec8acf2bc2bfa223d551
anton <antonio220022@mail.ru>
2025-09-03 00:13:46 +0900

    Юзер роутер

---
+++ app/bot/user/router.py
@@ -0,0 +1,22 @@
+from aiogram import Router, F
+from aiogram.exceptions import TelegramBadRequest
+from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, CallbackQuery
+
+from app.ai.agent import agent
+
+router = Router()
+
+#Start command
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer("Начнем")
+
+@router.message()
+async def say(message: Message):
+
+    ai_message = await agent.classify(message.text, message.from_user.id)
+
+    await message.answer(ai_message)
+
