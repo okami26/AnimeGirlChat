1c6ad2a00d24ac95a1a6d889b5fe7cf9355b89cd
anton <antonio220022@mail.ru>
2025-09-03 00:13:04 +0900

    Запуск бота

---
+++ app/bot/create_bot.py
@@ -0,0 +1,33 @@
+from aiogram import Dispatcher, Bot
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import BotCommand, BotCommandScopeDefault
+
+
+
+from app.bot.user.router import router as user_router
+from app.config import settings
+
+
+bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher(storage=MemoryStorage())
+
+
+async def set_commands():
+    commands = [BotCommand(command='start', description='Старт')]
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
+
+async def start_bot():
+    await set_commands()
+
+    dp.include_router(user_router)
+
+    await bot.set_webhook(settings.get_webhook)
+    await bot.send_message(settings.ADMIN_ID, text="Бот запущен!")
+
+async def stop_bot():
+    await bot.send_message(settings.ADMIN_ID, text="Бот остановлен!")
+    await bot.delete_webhook(drop_pending_updates=True)
+    await bot.session.close()
+
