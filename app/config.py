7b351148fe1a04ba459c18f8c2a6c710596e69c8
anton <antonio220022@mail.ru>
2025-09-03 17:18:02 +0900

    Привел config к стандарту

--- app/config.py
+++ app/config.py
@@ -15,6 +15,12 @@ class Settings(BaseSettings):
     BOT_TOKEN: str
     ADMIN_ID: str
     BASE_URL: str
+    FORMAT_LOG: str = "{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}"
+    LOG_ROTATION: str = "10 MB"
+
+    @property
+    def get_webhook(self) -> str:
+        return f"{self.BASE_URL}/webhook"
 
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
@@ -22,3 +28,10 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
+log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "log.txt")
+logger.add(
+    log_file_path,
+    format=settings.FORMAT_LOG,
+    level="INFO",
+    rotation=settings.LOG_ROTATION
+)
