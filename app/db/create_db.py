ff7d14bce920f44e1f6944aa9e5d5d27d13a5e57
anton <antonio220022@mail.ru>
2025-09-22 20:08:13 +0900

    Создание таблицы гс

--- app/db/create_db.py
+++ app/db/create_db.py
@@ -1,6 +1,6 @@
 import aiosqlite
 from app.config import settings
-
+from loguru import logger
 
 async def create_users_table():
     async with aiosqlite.connect(settings.DB_PATH) as db:
@@ -14,3 +14,20 @@ async def create_users_table():
         """
         )
         await db.commit()
+        logger.info("БД пользователей успешно создана")
+
+
+async def create_audios_table():
+    async with aiosqlite.connect(settings.DB_PATH) as db:
+        await db.execute(
+            """
+        CREATE TABLE IF NOT EXISTS audios (
+            id INTEGER PRIMARY KEY,
+            session_id INTEGER,
+            message_id INTEGER,
+            audio TEXT
+        );
+        """
+        )
+        await db.commit()
+        logger.info("БД аудио успешно создана")
